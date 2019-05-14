53558e722378e358672d150106d356dc6593da07
Tim <timothyhollabaugh@gmail.com>
2019-05-14 01:36:32 -0400

    Init database method and connection pool

--- src/lib.rs
+++ src/lib.rs
@@ -11,8 +11,40 @@ pub mod search;
 pub mod users;
 
 use serde::Serialize;
+use log::info;
+use log::warn;
+
+use diesel::r2d2::ConnectionManager;
+use diesel::MysqlConnection;
+use r2d2::Pool;
 
 use errors::Error;
+use errors::ErrorKind;
+
+embed_migrations!();
+
+pub fn init_database(
+    url: &str,
+) -> Result<Pool<ConnectionManager<MysqlConnection>>, Error> {
+    info!("Connecting to database");
+    let manager = ConnectionManager::new(url);
+
+    let pool = Pool::builder().max_size(15).build(manager);
+
+    let pool = match pool {
+        Ok(p) => p,
+        Err(e) => {
+            return Err(Error::with_source(ErrorKind::Database, Box::new(e)))
+        }
+    };
+
+    info!("Running migrations");
+    if let Err(e) = embedded_migrations::run(&pool.get()?) {
+        warn!("Could not run migrations: {}", e);
+    }
+
+    Ok(pool)
+}
 
 pub enum HttpMethod {
     GET,
